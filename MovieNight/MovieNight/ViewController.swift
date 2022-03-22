ab52ed26327ffb928e958787f0371d9befaf320e
AlexCheGH <alexchekushkin@Alexs-MacBook-Air.local>
2022-03-22 20:17:48 +0100

    base project

---
+++ MovieNight/MovieNight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by Alex Chekushkin on 3/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
